eb3d6a5ccf6e0d3165290df0bbce71b0f9b9cb5b
Tyuiu.ChepurnykhSR@outlook.com <winxsofa@bk.ru>
2023-09-21 23:22:28 +0500

    Task2

---
+++ Tyuiu.ChepurnykhSR.Sprint1.Task2.V14.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ChepurnykhSR.Sprint1.Task2.V14.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ChepurnykhSR.Sprint1.Task2.V14.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3c9946c7-abd9-4dae-90ee-40460c6545f1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
