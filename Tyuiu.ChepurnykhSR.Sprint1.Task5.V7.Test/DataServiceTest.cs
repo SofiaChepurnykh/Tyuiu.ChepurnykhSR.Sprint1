157095761b7cce5c4811ef4606878d4b5c660af0
Tyuiu.ChepurnykhSR@outlook.com <winxsofa@bk.ru>
2023-09-27 21:19:50 +0500

    Task5

---
+++ Tyuiu.ChepurnykhSR.Sprint1.Task5.V7.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.ChepurnykhSR.Sprint1.Task5.V7.Lib;
+
+namespace Tyuiu.ChepurnykhSR.Sprint1.Task5.V7.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            double f = 60;
+            DataService ds = new DataService();
+            double res = ds.AngleToHoursMinutes(f);
+
+            int result = Convert.ToInt32(res);
+
+            int wait = 2;
+            Assert.AreEqual(wait, result);
+        }
+    }
+}
---
+++ Tyuiu.ChepurnykhSR.Sprint1.Task5.V7/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ChepurnykhSR.Sprint1.Task5.V7.Lib;
+
+namespace Tyuiu.ChepurnykhSR.Sprint1.Task5.V7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Чепурных С. Р. | ИСТНб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт#1                                                                *");
+            Console.WriteLine("* Тема: Преобразование типов и класс Convert                              *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #7                                                              *");
+            Console.WriteLine("* Выполнила: Чепурных Софья Романовна | ИСТНб-23-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая решает задачу                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x;
+
+            Console.WriteLine("Введите значение F");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int res = Convert.ToInt32(ds.AngleToHoursMinutes(x));
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+        }
+    }
+}
